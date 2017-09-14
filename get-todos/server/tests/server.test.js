80e952708c5e1bd401db0e0b4dbcb37e81189b9f
Kamon Sreetha <kamon14838@outlook.com>
2017-09-14 12:21:34 +0700

    git-todos
    Finish project

--- get-todos/server/tests/server.test.js
+++ get-todos/server/tests/server.test.js
@@ -8,6 +8,30 @@ beforeEach((done) => {
   Todo.remove({}).then(() => done());
 });
 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+beforeEach((done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+});
+
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+  request(app
+  )
+  
+  .get('/todos') .expect(200) .expect((res) => {
+  expect(res.body.todos.length).toBe(2);
+  }).end(done);
+  
+  });
+  });
+
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
     var text = 'Test todo text';
